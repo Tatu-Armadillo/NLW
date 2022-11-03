c5e0163fec16a5376ddb29019a2097a9c6e5c4e6
Tatu-Armadillo <69278300+Tatu-Armadillo@users.noreply.github.com>
2022-11-03 17:41:15 -0300

    incorpoacao de componentes em telas

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -3,19 +3,22 @@ import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@
 import { THEME } from './src/styles/theme';
 
 import { Loading } from './src/components/Loading';
-import { SignIn } from './src/screens/Signin';
+import { Pools } from './src/screens/Pools';
+import { AuthContextProvider } from "./src/contexts/AuthContext";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      {fontsLoaded ? <Loading /> : <SignIn />}
+      <AuthContextProvider>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {fontsLoaded ? <Loading /> : <Pools />}
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
