6a6d04fff5d39fb5d00c23650a52c269620255b5
Tatu-Armadillo <69278300+Tatu-Armadillo@users.noreply.github.com>
2023-01-19 11:59:32 -0300

    aprofundando back, web e mobile

--- habits/mobile/App.tsx
+++ habits/mobile/App.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
 import {
   useFonts,
   Inter_400Regular,
@@ -6,7 +6,8 @@ import {
   Inter_700Bold,
   Inter_800ExtraBold
 } from '@expo-google-fonts/inter';
-import { Loading } from './src/src/components/Loading';
+import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -23,21 +24,9 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello World</Text>
-      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
-    </View>
+    <>
+      <Home />
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }, text: {
-    color: '#fff',
-    fontFamily: 'Inter_800ExtraBold'
-  }
-});
